659ae2d436138fec791a840d7cf65b0c05306c79
guilh <guilherme.egg@gmail.com>
2021-01-17 16:20:44 -0300

    Criação Categoria Entity e Categoria Repository

---
+++ back-end/challenge.api/src/main/java/com/hotmart/challenge/domain/model/entity/CategoriaEntity.java
@@ -0,0 +1,40 @@
+package com.hotmart.challenge.domain.model.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "TBCATEGORIA")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class CategoriaEntity implements Serializable{
+
+	private static final long serialVersionUID = 3148371629917712255L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	
+	@NotNull
+	@Size(max = 50)
+	@Column(name = "NOME")
+	private String nome;
+
+}
