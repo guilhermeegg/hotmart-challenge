76d708ee1932d162803d3bf50089ce17f75f22aa
guilh <guilherme.egg@gmail.com>
2021-01-17 18:33:40 -0300

    Correção VendaRepository - referência correta para a entidade.

--- back-end/challenge.api/src/main/java/com/hotmart/challenge/repository/VendaRepository.java
+++ back-end/challenge.api/src/main/java/com/hotmart/challenge/repository/VendaRepository.java
@@ -3,7 +3,9 @@ package com.hotmart.challenge.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hotmart.challenge.domain.model.entity.VendaEntity;
+
 @Repository
-public interface VendaRepository extends JpaRepository<VendaRepository, Long> {
+public interface VendaRepository extends JpaRepository<VendaEntity, Long> {
 
 }
