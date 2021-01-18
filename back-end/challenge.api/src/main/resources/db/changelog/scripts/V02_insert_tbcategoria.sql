7212eaa949d7b307fb3ca199bcc4d15f9b199ac9
guilh <guilherme.egg@gmail.com>
2021-01-18 14:33:53 -0300

    Carga de dados tbcategoria com base na newsapi.org

---
+++ back-end/challenge.api/src/main/resources/db/changelog/scripts/V02_insert_tbcategoria.sql
@@ -0,0 +1,9 @@
+insert into TBCATEGORIA(ID, NOME) values(1,'business');
+insert into TBCATEGORIA(ID, NOME) values(2,'entertainment');
+insert into TBCATEGORIA(ID, NOME) values(3,'general');
+insert into TBCATEGORIA(ID, NOME) values(4,'health');
+insert into TBCATEGORIA(ID, NOME) values(5,'science');
+insert into TBCATEGORIA(ID, NOME) values(6,'sports');
+insert into TBCATEGORIA(ID, NOME) values(7,'technology');
+
+
