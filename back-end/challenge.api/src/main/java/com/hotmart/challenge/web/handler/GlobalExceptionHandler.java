9745dde933d67565b70698a3b2939e1f8ef2e219
guilh <guilherme.egg@gmail.com>
2021-01-19 16:11:10 -0300

    Melhoria no Handler global para o tratamento de exceção.

--- back-end/challenge.api/src/main/java/com/hotmart/challenge/web/handler/GlobalExceptionHandler.java
+++ back-end/challenge.api/src/main/java/com/hotmart/challenge/web/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.hotmart.challenge.web.handler;
 
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 /**
  * Classe global para o tratamento de exceção
@@ -27,20 +28,24 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-
-		String mensagem = messageSource.getMessage("data.integrity.violation", null, LocaleContextHolder.getLocale());
-		Error error = new Error(ex.getLocalizedMessage(), mensagem);
-		return new ResponseEntity<>(error, new HttpHeaders(), HttpStatus.PRECONDITION_FAILED);
-
+		return getStandardResponseEntity("data.integrity.violation", HttpStatus.PRECONDITION_FAILED, ex);
 	}
 
 	@ExceptionHandler({ EmptyResultDataAccessException.class })
-	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
-			WebRequest request) {
-		String mensagem = messageSource.getMessage("empty.result.data.access.exception", null,
-				LocaleContextHolder.getLocale());
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex) {
+		return getStandardResponseEntity("empty.result.data.access.exception", HttpStatus.NOT_FOUND, ex);
+	}
+
+	@ExceptionHandler({ ConstraintViolationException.class })
+	public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException ex) {
+		return getStandardResponseEntity("constraint.violation.exception", HttpStatus.BAD_REQUEST, ex);
+	}
+
+	private ResponseEntity<Object> getStandardResponseEntity(String textoMensagem, HttpStatus httpStatus,
+			Exception ex) {
+		String mensagem = messageSource.getMessage(textoMensagem, null, LocaleContextHolder.getLocale());
 		Error error = new Error(ex.getLocalizedMessage(), mensagem);
-		return new ResponseEntity<>(error, new HttpHeaders(), HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(error, new HttpHeaders(), httpStatus);
 	}
 
 	public class Error {
