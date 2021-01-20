4cf7b55f324e2d2e4509faeb9a7af77e4dd8e972
guilh <guilherme.egg@gmail.com>
2021-01-20 17:15:22 -0300

    Carga de dados TBEXECUCAO_SERVICO

---
+++ back-end/challenge.api/src/main/resources/db/changelog/scripts/V08_insert_tbexecucao_servico.sql
@@ -0,0 +1,4 @@
+insert into TBEXECUCAO_SERVICO(ID, NOME_SERVICO) values(1,'Armazenar quantidade de notícias de cada categoria');
+insert into TBEXECUCAO_SERVICO(ID, NOME_SERVICO) values(2,'Armazenar score de cada produto');
+
+
