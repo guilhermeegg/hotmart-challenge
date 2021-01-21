cc506918e586d24c783e9e3d6a73886eb32f5d23
guilh <guilherme.egg@gmail.com>
2021-01-21 15:36:00 -0300

    Agendamento para armazenar o score do produto após start da aplicação

--- back-end/challenge.api/src/main/java/com/hotmart/challenge/ChallengeApiApplication.java
+++ back-end/challenge.api/src/main/java/com/hotmart/challenge/ChallengeApiApplication.java
@@ -1,13 +1,31 @@
 package com.hotmart.challenge;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import com.hotmart.challenge.scheduler.ServicoScoreProdutoScheduled;
 
 @SpringBootApplication
 public class ChallengeApiApplication {
 
+	@Autowired
+	private ServicoScoreProdutoScheduled scoreProdutoScheduled;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApiApplication.class, args);
+	}
+
+	/**
+	 * Evento após o start da aplicação. Agenda a execução imediata do
+	 * ServicoScoreProdutoScheduled
+	 * 
+	 */
+	@EventListener(ApplicationReadyEvent.class)
+	public void afterStartup() {
+		scoreProdutoScheduled.agendarExecucaoImediata();
 	}
 
 }
--- back-end/challenge.api/src/main/java/com/hotmart/challenge/scheduler/ServicoScoreProdutoScheduled.java
+++ back-end/challenge.api/src/main/java/com/hotmart/challenge/scheduler/ServicoScoreProdutoScheduled.java
@@ -1,6 +1,9 @@
 package com.hotmart.challenge.scheduler;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,23 +11,38 @@ import org.springframework.stereotype.Component;
 import com.hotmart.challenge.service.ProdutoService;
 
 /**
- * Agendador executado de hora em hora responsável por armazenar o score dos produtos
+ * Agendador executado de hora em hora responsável por armazenar o score dos
+ * produtos. Também fornece a possibilidade de agendar a execuação imediata da
+ * tarefa
  * 
  * @author Guilherme Egg
  *
  */
 @Component
-@EnableScheduling
 public class ServicoScoreProdutoScheduled {
 
 	@Autowired
-	ProdutoService produtoService;
+	private TaskScheduler scheduler;
 
-	private static final String TIME_ZONE = "America/Sao_Paulo";
+	@Autowired
+	private ProdutoService produtoService;
 
-	@Scheduled(cron = "0 0 */1 * * *", zone = TIME_ZONE)
-	public void executar() {
-		produtoService.armazenarScore();
+	/**
+	 * Agenda a execução imediata da tarefa QuantidadeNoticiasJob
+	 * 
+	 */
+	public void agendarExecucaoImediata() {
+		scheduler.schedule(new QuantidadeNoticiasJob(), new Date());
 	}
 
+	@EnableScheduling
+	private class QuantidadeNoticiasJob implements Runnable {
+		private static final String TIME_ZONE = "America/Sao_Paulo";
+
+		@Scheduled(cron = "0 0 */1 * * *", zone = TIME_ZONE)
+		public void run() {
+			produtoService.armazenarScore();
+		}
+
+	}
 }
