d91e0d06b8d9ae0064c83e66f65b13419e89c091
guilh <guilherme.egg@gmail.com>
2021-01-21 12:11:14 -0300

    Criação do método countByProduto na VendaRepository

--- back-end/challenge.api/src/main/java/com/hotmart/challenge/repository/VendaRepository.java
+++ back-end/challenge.api/src/main/java/com/hotmart/challenge/repository/VendaRepository.java
@@ -3,9 +3,12 @@ package com.hotmart.challenge.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hotmart.challenge.domain.model.entity.ProdutoEntity;
 import com.hotmart.challenge.domain.model.entity.VendaEntity;
 
 @Repository
 public interface VendaRepository extends JpaRepository<VendaEntity, Long> {
+
+	long countByProduto(ProdutoEntity produto);
 
 }
